ed7699e726f2a8c58af019faa06b78b9b9e2d2be
Sylwia Krzymowska <s.krzymowska@gmail.com>
2018-05-04 19:20:21 +0200

    Create new repsitory

---
+++ js/script.js
@@ -0,0 +1,11 @@
+var countElements = document.getElementsByClassName('button');
+
+console.log(countElements);
+
+function numberOfElements(n) {
+    var n = countElements.length;
+    for ( var i = 1 ; i <= n; i++) {
+         var x = document.getElementsByClassName('button').innerText;
+        }
+        console.log(x);
+    }
